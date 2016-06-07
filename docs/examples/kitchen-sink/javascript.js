860d367bd85f59361f17a70bfe4461734c4b286f
Stephen Wu <stephenwu95@gmail.com>
2016-06-07 15:14:50 -0700

    back

--- docs/examples/kitchen-sink/javascript.js
+++ docs/examples/kitchen-sink/javascript.js
@@ -39,10 +39,10 @@ angular
         var arr = JSON.parse(data);
         var i;
         arr.map(function (X) {
-          vm.events.push({title: X["Content Title"], type: 'important', startsAt: new Date (X["Store Date"]), endsAt: new Date (X["Store Date"]), draggable: true, resizable: true});
+          //vm.events.push({title: X["Content Title"], type: 'important', startsAt: new Date (X["Store Date"]), endsAt: new Date (X["Store Date"]), draggable: true, resizable: true});
         }
     }
-    //vm.events.push({title: 'WHATDDUP BITCHES', type: 'important', startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(), endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(), draggable: true, resizable: true});
+    vm.events.push({title: 'WHATDDUP BITCHES', type: 'important', startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(), endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(), draggable: true, resizable: true});
     
     vm.isCellOpen = false;
 
