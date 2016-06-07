08173e3a62a59c7e013ca98fbdaaae3a77e7ba68
Stephen Wu <stephenwu95@gmail.com>
2016-06-07 16:21:11 -0700

    events = []

--- docs/examples/kitchen-sink/javascript.js
+++ docs/examples/kitchen-sink/javascript.js
@@ -7,32 +7,7 @@ angular
     //These variables MUST be set as a minimum for the calendar to work
     vm.calendarView = 'month';
     vm.viewDate = new Date();
-    vm.events = [
-      {
-        title: 'An event',
-        type: 'warning',
-        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-        draggable: true,
-        resizable: true,
-        data: "SWAG TEST ONE two THREESWAG TEST ONE two THREESWAGTEST ONE two THREESWAG TEST ONE two THREESWAG TEST ONE two THREESWAG TEST ONE two THREESWAG TEST ONE two THREESWAG TEST ONE two THREESWAG TEST ONE two THREE"
-      }, {
-        title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-        type: 'info',
-        startsAt: moment().subtract(1, 'day').toDate(),
-        endsAt: moment().add(5, 'days').toDate(),
-        draggable: true,
-        resizable: true
-      }, {
-        title: 'This is a really long event title that occurs on every year',
-        type: 'important',
-        startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-        endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-        recursOn: 'year',
-        draggable: true,
-        resizable: true
-      }
-    ];
+    vm.events = [];
 
     //ADD EVENTS HERE
     $(document).ready(function(){
