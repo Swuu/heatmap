b8761583a16a4896397bea377ac7412576c0fe74
Stephen Wu <stephenwu95@gmail.com>
2016-06-30 17:05:16 -0700

    test

--- docs/examples/kitchen-sink/javascript.js
+++ docs/examples/kitchen-sink/javascript.js
@@ -141,4 +141,24 @@ angular
       $event.stopPropagation();
       event[field] = !event[field];
     };
+  })
+
+  .factory('alert', function($uibModal) {
+
+    function show(action, event) {
+      return $uibModal.open({
+        templateUrl: 'modalContent.html',
+        controller: function() {
+          var vm = this;
+          vm.action = action;
+          vm.event = event;
+        },
+        controllerAs: 'vm'
+      });
+    }
+
+    return {
+      show: show
+    };
+
   });
